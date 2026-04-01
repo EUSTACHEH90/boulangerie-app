0081d0557f79d48b722da25f910447378a5a309c
EUSTACHEH90 <eustachehema119@gmail.com>
2026-04-01 23:34:28 +0000

    Improve SEO

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,14 @@
 // app/layout.tsx
 import './globals.css'
+import type { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: "Boulagerie App",
+  description: "Application web d'une boulangerie artisanale avec gestion de produits, commandes et clients",
+  keywords: ["patisserie", "boulangerie", "web app", "next.js", "prisma", "typescript"],
+  authors: [{ name: "Eustache" }],
+  creator: "Eustache",
+};
 
 export default function RootLayout({
   children,
